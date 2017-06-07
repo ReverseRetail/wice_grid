6efbe2cae85306a40bfb0306fcc2c7e84a1bc2c5
Burkhard Vogel-Kreykenbohm <b.vogel@buddyandselly.com>
2017-06-07 09:57:06 +0200

    apply fix from Fabio Bonelli

--- lib/wice/wice_grid_core_ext.rb
+++ lib/wice/wice_grid_core_ext.rb
@@ -113,7 +113,12 @@ module ActionView #:nodoc:
   module Helpers #:nodoc:
     module TagHelper #:nodoc:
       def public_tag_options(options, escape = true) #:nodoc:
-        tag_options(options, escape)
+        if respond_to?(:tag_options)
+          tag_options(options, escape)
+        else
+          # Rails 5.1 uses TagBuilder
+          tag_builder.tag_options(options, escape)
+        end
       end
     end
   end
